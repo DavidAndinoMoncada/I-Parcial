cce6a116a3f231d1fcf42f7b9f37573e325746fa
DavidAndinoMoncada <dandinom@unah.hn>
2023-02-03 16:19:52 -0600

    Ejercicio_2
    Ejercicio de Funciones y Procedimientos

---
+++ Ejercicio_1/Ejercicio_1/Ejercicio_2.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Ejercicio_1
+{
+    partial class Ejercicio_2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Numero1textBox = new System.Windows.Forms.TextBox();
+            this.Numero2textBox = new System.Windows.Forms.TextBox();
+            this.OperacionescomboBox = new System.Windows.Forms.ComboBox();
+            this.Ejecutarbutton = new System.Windows.Forms.Button();
+            this.ResultadotextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(81, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero2";
+            // 
+            // Numero1textBox
+            // 
+            this.Numero1textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Numero1textBox.Location = new System.Drawing.Point(223, 66);
+            this.Numero1textBox.Name = "Numero1textBox";
+            this.Numero1textBox.Size = new System.Drawing.Size(210, 30);
+            this.Numero1textBox.TabIndex = 2;
+            // 
+            // Numero2textBox
+            // 
+            this.Numero2textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Numero2textBox.Location = new System.Drawing.Point(223, 132);
+            this.Numero2textBox.Name = "Numero2textBox";
+            this.Numero2textBox.Size = new System.Drawing.Size(210, 30);
+            this.Numero2textBox.TabIndex = 3;
+            // 
+            // OperacionescomboBox
+            // 
+            this.OperacionescomboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OperacionescomboBox.FormattingEnabled = true;
+            this.OperacionescomboBox.Items.AddRange(new object[] {
+            "Suma",
+            "Resta",
+            "Multiplicación",
+            "División",
+            "Par"});
+            this.OperacionescomboBox.Location = new System.Drawing.Point(223, 199);
+            this.OperacionescomboBox.Name = "OperacionescomboBox";
+            this.OperacionescomboBox.Size = new System.Drawing.Size(208, 33);
+            this.OperacionescomboBox.TabIndex = 4;
+            // 
+            // Ejecutarbutton
+            // 
+            this.Ejecutarbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ejecutarbutton.Location = new System.Drawing.Point(252, 262);
+            this.Ejecutarbutton.Name = "Ejecutarbutton";
+            this.Ejecutarbutton.Size = new System.Drawing.Size(159, 35);
+            this.Ejecutarbutton.TabIndex = 5;
+            this.Ejecutarbutton.Text = "Ejecutar";
+            this.Ejecutarbutton.UseVisualStyleBackColor = true;
+            this.Ejecutarbutton.Click += new System.EventHandler(this.Ejecutarbutton_Click);
+            // 
+            // ResultadotextBox
+            // 
+            this.ResultadotextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultadotextBox.Location = new System.Drawing.Point(223, 331);
+            this.ResultadotextBox.Name = "ResultadotextBox";
+            this.ResultadotextBox.Size = new System.Drawing.Size(210, 30);
+            this.ResultadotextBox.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(81, 334);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Resultado";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Ejercicio_2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 476);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ResultadotextBox);
+            this.Controls.Add(this.Ejecutarbutton);
+            this.Controls.Add(this.OperacionescomboBox);
+            this.Controls.Add(this.Numero2textBox);
+            this.Controls.Add(this.Numero1textBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Ejercicio_2";
+            this.Text = "Ejercicio_2";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Numero1textBox;
+        private System.Windows.Forms.TextBox Numero2textBox;
+        private System.Windows.Forms.ComboBox OperacionescomboBox;
+        private System.Windows.Forms.Button Ejecutarbutton;
+        private System.Windows.Forms.TextBox ResultadotextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ Ejercicio_1/Ejercicio_1/Ejercicio_2.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_1
+{
+    public partial class Ejercicio_2 : Form
+    {
+
+        decimal ResultadoFinal = 0;
+        public Ejercicio_2()
+        {
+            InitializeComponent();
+        }
+
+        private void Ejecutarbutton_Click(object sender, EventArgs e)
+        {
+            if (Numero1textBox.Text == "")
+            {
+                errorProvider1.SetError(Numero1textBox, "Ingrese un Valor");
+                return;
+            }
+            if (Numero2textBox.Text == "")
+            {
+                errorProvider1.SetError(Numero2textBox, "Ingrese un Valor");
+                return;
+            }
+            if (OperacionescomboBox.Text == "")
+            {
+                errorProvider1.SetError(OperacionescomboBox, " Seleccione una Opcion");
+                return;
+            }
+
+            decimal numero1 = Convert.ToDecimal(Numero1textBox.Text);
+            decimal numero2 = Convert.ToDecimal(Numero2textBox.Text);
+
+            ResultadoFinal = Ejecutar(numero1, numero2);
+
+            ResultadotextBox.Text = Convert.ToString(ResultadoFinal);
+
+            MessageBox.Show("El Resultado es: " + ResultadoFinal, "Mensaje ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private decimal Ejecutar(decimal n1, decimal n2)
+        {
+            string Operacion = OperacionescomboBox.Text;
+            decimal resultado = 0;
+
+            if (Operacion == "Suma")
+            {
+                resultado = n1 + n2;
+            }
+            else if (Operacion == "Resta")
+            {
+                resultado = n1 - n2;
+            }
+            else if (Operacion == "Multiplicación")
+            {
+                resultado = n1 * n2;
+            }
+            else if (Operacion == "División")
+            {
+                resultado = n1 / n2;
+            }
+            else if (Operacion == "Par")
+            {
+                resultado = (n1 + n2) % 2;
+            }
+
+            return resultado;
+        }
+
+        private decimal Ejecutar2(decimal n1, decimal n2)
+        {
+            string Operacion = OperacionescomboBox.Text;
+            decimal resultado = 0;
+
+            if (Operacion == "Suma")
+                resultado = n1 + n2;
+            else if (Operacion == "Resta")
+                resultado = n1 - n2;
+            else if (Operacion == "Multiplicación")
+                resultado = n1 * n2;
+            else if (Operacion == "División")
+                resultado = n1 / n2;
+            else if (Operacion == "Par")
+                resultado = (n1 + n2) % 2;
+
+            return resultado;
+
+        }
+    }
+}
