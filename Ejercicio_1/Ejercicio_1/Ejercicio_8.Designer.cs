c619cb36338077d25ccc4a2f96522b154ef1ab8e
DavidAndinoMoncada <dandinom@unah.hn>
2023-02-14 04:28:14 -0600

    Ejercicio_8
    Funcion de Recursividad

---
+++ Ejercicio_1/Ejercicio_1/Ejercicio_8.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Ejercicio_1
+{
+    partial class Ejercicio_8
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Ejecutarbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(36, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(218, 500);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Ejecutarbutton
+            // 
+            this.Ejecutarbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ejecutarbutton.Location = new System.Drawing.Point(303, 47);
+            this.Ejecutarbutton.Name = "Ejecutarbutton";
+            this.Ejecutarbutton.Size = new System.Drawing.Size(134, 57);
+            this.Ejecutarbutton.TabIndex = 1;
+            this.Ejecutarbutton.Text = "Ejecutar";
+            this.Ejecutarbutton.UseVisualStyleBackColor = true;
+            this.Ejecutarbutton.Click += new System.EventHandler(this.Ejecutarbutton_Click);
+            // 
+            // Ejercicio_8
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 531);
+            this.Controls.Add(this.Ejecutarbutton);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Ejercicio_8";
+            this.Text = "Ejercicio_8";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Ejecutarbutton;
+    }
+}
---
+++ Ejercicio_1/Ejercicio_1/Ejercicio_8.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_1
+{
+    public partial class Ejercicio_8 : Form
+    {
+        public Ejercicio_8()
+        {
+            InitializeComponent();
+        }
+
+        private void Ejecutarbutton_Click(object sender, EventArgs e)
+        {
+            RecorrerDeUnoEnUno(1, 100);
+        }
+
+        private void RecorrerDeUnoEnUno(int valinicial, int valfinal)
+        {
+            listBox1.Items.Add(valinicial);
+
+            if (valinicial < valfinal)
+            {
+                RecorrerDeUnoEnUno(valinicial + 1, valfinal);
+            }
+
+        }
+
+    }
+}
